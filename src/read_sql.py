eae57a49d48d68f05ab812bfbaf6b7f6322e04c0
Larrygf02 <raulgf_02@hotmail.com>
2020-06-19 23:58:30 -0500

    Pandas read_sql

---
+++ src/read_sql.py
@@ -0,0 +1,5 @@
+from sqlalchemy import create_engine
+import pandas as pd
+engine = create_engine('postgresql://postgres:123@localhost:5432/bloggo')
+df = pd.read_sql_query("SELECT * FROM users", con=engine)
+print(df)
