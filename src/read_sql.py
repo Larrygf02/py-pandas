f67241e4a6442d7589f211ca977cd10a68d09ab2
Larrygf02 <raulgf_02@hotmail.com>
2020-06-24 21:37:01 -0500

    get columns and value

--- src/read_sql.py
+++ src/read_sql.py
@@ -2,4 +2,6 @@ from sqlalchemy import create_engine
 import pandas as pd
 engine = create_engine('postgresql://postgres:123@localhost:5432/bloggo')
 df = pd.read_sql_query("SELECT * FROM users", con=engine)
+df.columns.list()
+df.values
 print(df)
