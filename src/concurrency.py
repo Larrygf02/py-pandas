59c62ec43fcf0e2336433c94a54fb244eec83430
Larrygf02 <raulgf_02@hotmail.com>
2020-06-25 23:31:33 -0500

    concurrency with python +1

--- src/concurrency.py
+++ src/concurrency.py
@@ -7,17 +7,17 @@ r = Parallel(n_jobs=10, verbose=100)(delayed(sleep)(.2) for _ in range(10))
 from pandas import DataFrame
 import pandas as pd
 import numpy as np
-Cars = {'car': ['Honda Civic','Toyota Corolla','Ford Focus','Audi A4'],
-        'price': [22000,25000,27000,35000]
-}
+import time
+start_time = time.time()
 engine = create_engine('postgresql://postgres:123@localhost:5432/cardb')
-df = DataFrame(Cars, columns= ['car', 'price'])
-#df.to_sql('cars', con=engine, if_exists='replace')
-print(df)
 N = 500000
 cars = pd.util.testing.rands_array(10, N)
 prices = np.random.randint(5,30,size=N)
-df = pd.DataFrame()
-df['car'] = cars
-df['price'] = prices
-df.to_sql('cars', con=engine, if_exists='replace')
+data = pd.DataFrame()
+data['car'] = cars
+data['price'] = prices
+split_n = 10000
+for i, df in data.groupby(np.arange(len(data)) // split_n):
+    print(df.shape)
+#df.to_sql('cars', con=engine, if_exists='replace')
+print("Execution success in %s " % (time.time() - start_time))
