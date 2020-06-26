eb306061588196a10b9f14f8b8a93fc4292cff58
Larrygf02 <raulgf_02@hotmail.com>
2020-06-25 23:47:57 -0500

    Insert multiple data

--- src/concurrency.py
+++ src/concurrency.py
@@ -2,7 +2,7 @@ from time import sleep
 from joblib import Parallel, delayed
 from sqlalchemy import create_engine
 
-r = Parallel(n_jobs=10, verbose=100)(delayed(sleep)(.2) for _ in range(10))
+#r = Parallel(n_jobs=10, verbose=100)(delayed(sleep)(.2) for _ in range(10))
 
 from pandas import DataFrame
 import pandas as pd
@@ -10,14 +10,18 @@ import numpy as np
 import time
 start_time = time.time()
 engine = create_engine('postgresql://postgres:123@localhost:5432/cardb')
-N = 500000
+N = 5000
 cars = pd.util.testing.rands_array(10, N)
 prices = np.random.randint(5,30,size=N)
 data = pd.DataFrame()
 data['car'] = cars
 data['price'] = prices
-split_n = 10000
-for i, df in data.groupby(np.arange(len(data)) // split_n):
+split_n = 1000
+def insert_df(df):
+    df.to_sql('cars', con=engine, if_exists='replace')
+group_data = data.groupby(np.arange(len(data)) // split_n)
+#r = Parallel(n_jobs=10, verbose=100)(delayed(insert_df(df)) for _,df in group_data)
+for i, df in group_data:
+    insert_df(df)
     print(df.shape)
-#df.to_sql('cars', con=engine, if_exists='replace')
 print("Execution success in %s " % (time.time() - start_time))
