692433df7b42028304bcfafc4a379a94c38c233f
Larrygf02 <raulgf_02@hotmail.com>
2020-06-26 20:27:16 -0500

    Merge pandas

--- src/concurrency.py
+++ src/concurrency.py
@@ -1,6 +1,7 @@
 from time import sleep
 from joblib import Parallel, delayed
 from sqlalchemy import create_engine
+from loky import wrap_non_picklable_objects
 
 #r = Parallel(n_jobs=10, verbose=100)(delayed(sleep)(.2) for _ in range(10))
 
@@ -17,11 +18,14 @@ data = pd.DataFrame()
 data['car'] = cars
 data['price'] = prices
 split_n = 1000
+
+@delayed
+@wrap_non_picklable_objects
 def insert_df(df):
     df.to_sql('cars', con=engine, if_exists='replace')
 group_data = data.groupby(np.arange(len(data)) // split_n)
-#r = Parallel(n_jobs=10, verbose=100)(delayed(insert_df(df)) for _,df in group_data)
-for i, df in group_data:
+r = Parallel(n_jobs=10)(delayed(insert_df)(df) for _,df in group_data)
+""" for i, df in group_data:
     insert_df(df)
-    print(df.shape)
+    print(df.shape) """
 print("Execution success in %s " % (time.time() - start_time))
